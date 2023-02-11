9858eb5d6319c733860aa1e50e005ea38d78dcf3
kasterra <kasterra@naver.com>
2023-02-11 15:09:08 +0900

    redirect to naver.com

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -167,7 +167,7 @@ const Result = () => {
               transform: "translateY(2px)",
               boxShadow: "lg",
             }}
-            onClick={() => navigate("#")}
+            onClick={() => window.location.replace("https://naver.com")}
           >
             사역국 지원하러 가기
           </Button>
